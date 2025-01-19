cb5cff137be1289ef8db2d08e7bbf4b2d13126c1
CaiNiaoE <rookieyi@outlook.com>
2025-01-19 11:23:27 +0800

    refactor: Improve Zig Implementation
    Refactored variable names
    Organized related variables into GameState struct for better readability and maintainability
    Split game logic into modular functions

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,15 +1,27 @@
 const rl = @import("raylib");
 
-const SNAKE_LENGTH = 256;
-const SQUARE_SIZE = 31;
+const screen_width = 800;
+const screen_height = 450;
+const square_size = 32;
+const snake_length = 256;
+const offset = rl.Vector2{
+    .x = screen_width % square_size,
+    .y = screen_height % square_size,
+};
 
 const Snake = struct {
     position: rl.Vector2,
     size: rl.Vector2,
     speed: rl.Vector2,
     color: rl.Color,
-    pub fn init(position: rl.Vector2, size: rl.Vector2, speed: rl.Vector2, color: rl.Color) Snake {
-        return Snake{ .position = position, .size = size, .speed = speed, .color = color };
+
+    pub fn init(x: f32, y: f32, color: rl.Color) Snake {
+        return Snake{
+            .position = rl.Vector2{ .x = x, .y = y },
+            .size = rl.Vector2{ .x = square_size, .y = square_size },
+            .speed = rl.Vector2{ .x = square_size, .y = 0 },
+            .color = color,
+        };
     }
 };
 
@@ -19,155 +31,189 @@ const Food = struct {
     active: bool,
     color: rl.Color,
 };
-const screenWidth = 800;
-const screenHeight = 450;
 
-var framesCount: i32 = 0;
-var gameOver = false;
-var pause = false;
+var game_state = struct {
+    frames_count: i32 = 0,
+    game_over: bool = false,
+    pause: bool = false,
+    current_snake_length: usize = 1,
+    fruit: Food = Food{
+        .active = false,
+        .color = rl.Color.sky_blue,
+        .position = rl.Vector2.init(0, 0),
+        .size = rl.Vector2{ .x = square_size, .y = square_size },
+    },
+    snake: [snake_length]Snake = .{Snake.init(offset.x / 2, offset.y / 2, rl.Color.black)} ** snake_length,
+    snake_positions: [snake_length]rl.Vector2 = .{rl.Vector2.init(0, 0)} ** snake_length,
+}{};
 
-var fruit = Food{ .active = false, .color = rl.Color.black, .position = rl.Vector2.init(0, 0), .size = rl.Vector2.init(0, 0) };
-var snake: [SNAKE_LENGTH]Snake = .{Snake.init(rl.Vector2.init(0, 0), rl.Vector2.init(0, 0), rl.Vector2.init(0, 0), rl.Color.black)} ** SNAKE_LENGTH;
-var snakePosition: [SNAKE_LENGTH]rl.Vector2 = .{rl.Vector2.init(0, 0)} ** SNAKE_LENGTH;
-
-var allowMove = false;
-var offset = rl.Vector2{ .x = 0, .y = 0 };
-
-var counterTail: usize = 0;
-
-pub fn main() anyerror!void {
-    rl.initWindow(screenWidth, screenHeight, "Snake");
+pub fn main() !void {
+    rl.initWindow(screen_width, screen_height, "Snake");
     defer rl.closeWindow();
 
     initGame();
     rl.setTargetFPS(60);
+
     while (!rl.windowShouldClose()) {
         updateDrawFrame();
     }
 }
+
 fn initGame() void {
-    framesCount = 0;
-    gameOver = false;
-    pause = false;
+    game_state.frames_count = 0;
+    game_state.game_over = false;
+    game_state.pause = false;
+    game_state.current_snake_length = 1;
 
-    counterTail = 1;
-    allowMove = false;
-    offset.x = screenWidth % SQUARE_SIZE;
-    offset.y = screenHeight % SQUARE_SIZE;
-
-    for (&snake, 0..) |*snakeBlock, i| {
-        snakeBlock.position = rl.Vector2{ .x = offset.x / 2, .y = offset.y / 2 };
-        snakeBlock.size = rl.Vector2{ .x = SQUARE_SIZE, .y = SQUARE_SIZE };
-        snakeBlock.speed = rl.Vector2{ .x = SQUARE_SIZE, .y = 0 };
-        if (i == 0) {
-            snakeBlock.color = rl.Color.black;
-        } else {
-            snakeBlock.color = rl.Color.blue;
-        }
+    game_state.snake[0] = Snake.init(offset.x / 2, offset.y / 2, rl.Color.black);
+    for (1..snake_length) |i| {
+        game_state.snake[i] = Snake.init(0, 0, rl.Color.blue);
     }
-    for (&snakePosition) |*entry| {
+    for (&game_state.snake_positions) |*entry| {
         entry.* = rl.Vector2{ .x = 0, .y = 0 };
     }
-    fruit.size = rl.Vector2{ .x = SQUARE_SIZE, .y = SQUARE_SIZE };
-    fruit.color = rl.Color.sky_blue;
-    fruit.active = false;
+
+    game_state.fruit.active = false;
 }
+
 fn updateGame() void {
-    if (!gameOver) {
-        if (rl.isKeyPressed(.p)) {
-            pause = !pause;
-        }
-        if (!pause) {
-            if (rl.isKeyPressed(.right) and (snake[0].speed.x == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{ .x = SQUARE_SIZE, .y = 0 };
-                allowMove = false;
-            }
-            if (rl.isKeyPressed(.left) and (snake[0].speed.x == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{ .x = -SQUARE_SIZE, .y = 0 };
-                allowMove = false;
-            }
-            if (rl.isKeyPressed(.up) and (snake[0].speed.y == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{ .x = 0, .y = -SQUARE_SIZE };
-                allowMove = false;
-            }
-            if (rl.isKeyPressed(.down) and (snake[0].speed.y == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{ .x = 0, .y = SQUARE_SIZE };
-                allowMove = false;
-            }
-
-            for (0..counterTail) |i| {
-                snakePosition[i] = snake[i].position;
-            }
-            if (@mod(framesCount, 5) == 0) {
-                for (0..counterTail) |i| {
-                    if (i == 0) {
-                        snake[0].position.x += snake[0].speed.x;
-                        snake[0].position.y += snake[0].speed.y;
-                        allowMove = true;
-                    } else {
-                        snake[i].position = snakePosition[i - 1];
-                    }
-                }
-            }
-            if ((snake[0].position.x > (screenWidth - offset.x)) or (snake[0].position.y > (screenHeight - offset.y)) or (snake[0].position.x < 0) or (snake[0].position.y < 0)) {
-                gameOver = true;
-            }
-
-            for (1..counterTail) |i| {
-                if ((snake[0].position.x == snake[i].position.x) and (snake[0].position.y == snake[i].position.y)) {
-                    gameOver = true;
-                }
-            }
-
-            if (!fruit.active) {
-                fruit.active = true;
-                fruit.position = rl.Vector2{ .x = @as(f32, @floatFromInt(rl.getRandomValue(0, (screenWidth / SQUARE_SIZE) - 1))) * SQUARE_SIZE + offset.x / 2, .y = @as(f32, @floatFromInt(rl.getRandomValue(0, (screenHeight / SQUARE_SIZE) - 1))) * SQUARE_SIZE + offset.y / 2 };
-                var i: usize = 0;
-                while (i < counterTail) : (i += 1) {
-                    if ((fruit.position.x == snake[i].position.x) and (fruit.position.y == snake[i].position.y)) {
-                        fruit.position = rl.Vector2{ .x = @as(f32, @floatFromInt(rl.getRandomValue(0, (screenWidth / SQUARE_SIZE) - 1))) * SQUARE_SIZE + offset.x / 2, .y = @as(f32, @floatFromInt(rl.getRandomValue(0, (screenHeight / SQUARE_SIZE) - 1))) * SQUARE_SIZE + offset.y / 2 };
-                        i = 0;
-                    }
-                }
-            }
-            if ((snake[0].position.x < (fruit.position.x + fruit.size.x)) and ((snake[0].position.x + snake[0].size.x) > fruit.position.x) and (snake[0].position.y < (fruit.position.y + fruit.size.y)) and ((snake[0].position.y + snake[0].size.y) > fruit.position.y)) {
-                snake[counterTail].position = snakePosition[counterTail - 1];
-                counterTail += 1;
-                fruit.active = false;
-            }
-            framesCount += 1;
-        }
-    } else {
+    if (game_state.game_over) {
         if (rl.isKeyPressed(.enter)) {
             initGame();
-            gameOver = false;
+        }
+        return;
+    }
+
+    handleInput();
+    updateSnake();
+    checkCollisions();
+    game_state.frames_count += 1;
+}
+
+fn handleInput() void {
+    if (rl.isKeyPressed(.p)) {
+        game_state.pause = !game_state.pause;
+    }
+    if (game_state.pause) return;
+
+    const input = rl.getKeyPressed();
+    switch (input) {
+        .right => {
+            if (game_state.snake[0].speed.x == 0) game_state.snake[0].speed = rl.Vector2{ .x = square_size, .y = 0 };
+        },
+        .left => {
+            if (game_state.snake[0].speed.x == 0) game_state.snake[0].speed = rl.Vector2{ .x = -square_size, .y = 0 };
+        },
+        .up => {
+            if (game_state.snake[0].speed.y == 0) game_state.snake[0].speed = rl.Vector2{ .x = 0, .y = -square_size };
+        },
+        .down => {
+            if (game_state.snake[0].speed.y == 0) game_state.snake[0].speed = rl.Vector2{ .x = 0, .y = square_size };
+        },
+        else => {},
+    }
+}
+
+fn updateSnake() void {
+    for (0..game_state.current_snake_length) |i| {
+        game_state.snake_positions[i] = game_state.snake[i].position;
+    }
+
+    if (@mod(game_state.frames_count, 5) == 0) {
+        for (0..game_state.current_snake_length) |i| {
+            if (i == 0) {
+                game_state.snake[0].position.x += game_state.snake[0].speed.x;
+                game_state.snake[0].position.y += game_state.snake[0].speed.y;
+            } else {
+                game_state.snake[i].position = game_state.snake_positions[i - 1];
+            }
         }
     }
 }
-fn DrawGame() void {
+
+fn checkCollisions() void {
+    if (game_state.snake[0].position.x < 0 or game_state.snake[0].position.x > screen_width - offset.x or
+        game_state.snake[0].position.y < 0 or game_state.snake[0].position.y > screen_height - offset.y)
+    {
+        game_state.game_over = true;
+    }
+
+    for (1..game_state.current_snake_length) |i| {
+        if (game_state.snake[0].position.x == game_state.snake[i].position.x and game_state.snake[0].position.y == game_state.snake[i].position.y) {
+            game_state.game_over = true;
+        }
+    }
+
+    if (!game_state.fruit.active) {
+        game_state.fruit.position = randomPosition();
+        var index: usize = 0;
+        while (index < game_state.current_snake_length) : (index += 1) {
+            if (rl.checkCollisionRecs(.{ .x = game_state.snake[index].position.x, .y = game_state.snake[index].position.y, .width = game_state.snake[index].size.x, .height = game_state.snake[index].size.y }, .{ .x = game_state.fruit.position.x, .y = game_state.fruit.position.y, .width = game_state.fruit.size.x, .height = game_state.fruit.size.y })) {
+                game_state.fruit.position = randomPosition();
+                index = 0;
+            }
+        }
+        game_state.fruit.active = true;
+    }
+
+    if (rl.checkCollisionRecs(.{ .x = game_state.snake[0].position.x, .y = game_state.snake[0].position.y, .width = game_state.snake[0].size.x, .height = game_state.snake[0].size.y }, .{ .x = game_state.fruit.position.x, .y = game_state.fruit.position.y, .width = game_state.fruit.size.x, .height = game_state.fruit.size.y })) {
+        game_state.snake[game_state.current_snake_length].position = game_state.snake_positions[game_state.current_snake_length - 1];
+        game_state.current_snake_length += 1;
+        game_state.fruit.active = false;
+    }
+}
+
+fn updateDrawFrame() void {
+    updateGame();
+    drawGame();
+}
+
+fn drawGame() void {
     rl.beginDrawing();
     defer rl.endDrawing();
     rl.clearBackground(rl.Color.ray_white);
-    if (!gameOver) {
-        for (0..screenWidth / SQUARE_SIZE + 1) |i| {
-            rl.drawLineV(rl.Vector2{ .x = @as(f32, @floatFromInt(SQUARE_SIZE * i)) + offset.x / 2, .y = offset.y / 2 }, rl.Vector2{ .x = @as(f32, @floatFromInt(SQUARE_SIZE * i)) + offset.x / 2, .y = screenHeight - offset.y / 2 }, rl.Color.light_gray);
-        }
-        for (0..screenHeight / SQUARE_SIZE + 1) |i| {
-            rl.drawLineV(rl.Vector2{ .x = offset.x / 2, .y = @as(f32, @floatFromInt(SQUARE_SIZE * i)) + offset.y / 2 }, rl.Vector2{ .x = screenWidth - offset.x / 2, .y = @as(f32, @floatFromInt(SQUARE_SIZE * i)) + offset.y / 2 }, rl.Color.light_gray);
-        }
 
-        for (0..counterTail) |i| {
-            rl.drawRectangleV(snake[i].position, snake[i].size, snake[i].color);
-        }
-        rl.drawRectangleV(fruit.position, fruit.size, fruit.color);
-        if (pause) {
-            rl.drawText("GAME PAUSED", screenWidth / 2 - @divExact(rl.measureText("GAME PAUSED", 40), 2), screenHeight / 2 - 40, 40, rl.Color.gray);
-        }
-    } else {
-        rl.drawText("PRESS [ENTER] TO PLAY AGAIN", @divExact(rl.getScreenWidth(), 2) - @divExact(rl.measureText("PRESS [ENTER] TO PLAY AGAIN", 20), 2), @divExact(rl.getScreenHeight(), 2) - 50, 20, rl.Color.gray);
+    if (game_state.game_over) {
+        rl.drawText(
+            "PRESS [ENTER] TO PLAY AGAIN",
+            screen_width / 2 - @divExact(rl.measureText("PRESS [ENTER] TO PLAY AGAIN", 20), 2),
+            screen_height / 2 - 50,
+            20,
+            rl.Color.gray,
+        );
+        return;
+    }
+
+    drawGrid();
+
+    for (0..game_state.current_snake_length) |i| {
+        rl.drawRectangleV(game_state.snake[i].position, game_state.snake[i].size, game_state.snake[i].color);
+    }
+
+    rl.drawRectangleV(game_state.fruit.position, game_state.fruit.size, game_state.fruit.color);
+
+    if (game_state.pause) {
+        rl.drawText(
+            "GAME PAUSED",
+            screen_width / 2 - @divExact(rl.measureText("GAME PAUSED", 40), 2),
+            screen_height / 2 - 40,
+            40,
+            rl.Color.gray,
+        );
     }
 }
-fn updateDrawFrame() void {
-    updateGame();
-    DrawGame();
+fn drawGrid() void {
+    for (0..screen_width / square_size + 1) |i| {
+        rl.drawLineV(rl.Vector2{ .x = @as(f32, @floatFromInt(square_size * i)) + offset.x / 2, .y = offset.y / 2 }, rl.Vector2{ .x = @as(f32, @floatFromInt(square_size * i)) + offset.x / 2, .y = screen_height - offset.y / 2 }, rl.Color.light_gray);
+    }
+    for (0..screen_height / square_size + 1) |i| {
+        rl.drawLineV(rl.Vector2{ .x = offset.x / 2, .y = @as(f32, @floatFromInt(square_size * i)) + offset.y / 2 }, rl.Vector2{ .x = screen_width - offset.x / 2, .y = @as(f32, @floatFromInt(square_size * i)) + offset.y / 2 }, rl.Color.light_gray);
+    }
+}
+
+fn randomPosition() rl.Vector2 {
+    return rl.Vector2{
+        .x = @as(f32, @floatFromInt(rl.getRandomValue(0, screen_width / square_size - 1))) * square_size + offset.x / 2,
+        .y = @as(f32, @floatFromInt(rl.getRandomValue(0, screen_height / square_size - 1))) * square_size + offset.y / 2,
+    };
 }
